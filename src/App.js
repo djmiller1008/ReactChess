d60bfe540435c26ab4c30b9dcd1ab80c542b8152
David <djmiller1008@yahoo.com>
2022-08-26 10:16:51 +0800

    Remove existing code not related to this project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import { ThemeProvider } from 'styled-components';
-import {Container} from './components/styled/Container.styled';
-import Header from './components/Header';
-import GlobalStyles from './components/styled/Global';
+import Board from "./components/board";
 
 
-const theme = {
-  colors: {
-    header: '#ebfbff',
-    body: '#fff',
-    footer: '#003333',
-  },
-}
+
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <>
-      <GlobalStyles />
-      <Header />
-        <Container>
-          <h1>Hello World</h1>
-        </Container>
-      </>
-    </ThemeProvider>
+    <Board />
   );
 }
 
