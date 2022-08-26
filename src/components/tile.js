b999e6cec3bf104a740ef54b909293270e5fee1a
David <djmiller1008@yahoo.com>
2022-08-26 10:18:11 +0800

    Add tile component that contains no piece

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import '../assets/styles/tile.css'
 
-export default function tile() {
+
+export default function Tile() {
   return (
-    <div>tile</div>
+    <div className='tile-container'>
+        
+    </div>
   )
 }
