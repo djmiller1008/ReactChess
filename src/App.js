e95b7de046d5ec0dc1a612270b966b9746e693ae
David <djmiller1008@yahoo.com>
2022-08-26 23:20:08 +0800

    Wrap Board component in Dnd provider as per documentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Board from "./components/board";
-
-
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 
 function App() {
   return (
-    <Board />
+    <DndProvider backend={HTML5Backend}>
+      <Board />
+    </DndProvider>
   );
 }
 
