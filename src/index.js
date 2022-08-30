18605e26fd7afd0220e62aa0c8b7f70f747e4d91
David <djmiller1008@yahoo.com>
2022-08-30 23:17:17 +0800

    Reconfigure index to observe the state of the board

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Board from './components/board'
+import { observeBoard } from './components/game'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
- 
-);
+const root = document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+observeBoard((knightPositions) =>
+  ReactDOM.render(<Board knightPositions={knightPositions} />, root)
+)
+
+
