9be0e07d48f5e7738aea3223b22ffbae7cffcd89
David <djmiller1008@yahoo.com>
2022-08-31 12:48:34 +0800

    Change render method for board, add DND provider for drag and drop functionality

--- src/components/board.js
+++ src/components/board.js
@@ -1,85 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import Rook from './pieces/rook';
-import Knight from './pieces/knight';
-import Bishop from './pieces/bishop';
-import Queen from './pieces/queen';
-import Pawn from './pieces/pawn';
-import King from './pieces/king';
-import Tile from './tile';
-import BlackPawn from '../assets/images/Pawn_black.svg';
-import BlackKing from '../assets/images/King_black.svg';
-import BlackQueen from '../assets/images/Queen_black.svg';
-import BlackBishop from '../assets/images/Bishop_black.svg';
-import BlackKnight from '../assets/images/Knight_black.svg';
-import BlackRook from '../assets/images/Rook_black.svg';
-import WhitePawn from '../assets/images/Pawn_white.svg';
-import WhiteRook from '../assets/images/Rook_white.svg';
-import WhiteKnight from '../assets/images/Knight_white.svg';
-import WhiteBishop from '../assets/images/Bishop_white.svg';
-import WhiteQueen from '../assets/images/Queen_white.svg';
-import WhiteKing from '../assets/images/King_white.svg';
+import React from "react";
+import Tile from "./tile";
+import Knight from "./pieces/knight";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import '../assets/styles/board.css';
+import { moveKnight, canMoveKnight } from "./game";
 
-const BOARD_SIZE = 8;
+export default function Board({ boardPieces }) {
 
-export default function Board() {
-  const [board, setBoard] = useState([]);
+    function renderSquare(x, y, boardPieces) {
+        const black = (x + y) % 2 === 1;
+        
+        let pieceColor = null;
+        let isKnightHere = null;
+        Object.keys(boardPieces).forEach(piece => {
+            boardPieces[piece].forEach((pieceInfo, i) => {
+                if (x === pieceInfo[0] && y === pieceInfo[1]) {
+                    isKnightHere = true;
+                    pieceColor = pieceInfo[2];
+                }
+            })
+        })
+        
+        const piece = isKnightHere ? <Knight pieceColor={pieceColor}  pos={[x, y]} /> : null;
 
-  useEffect(() => {
-    setupBoard();
-  }, []);
-  
-
-  const setupBoard = () => {
-    let grid = [];
-    for (let i = 0; i < BOARD_SIZE; i++) {
-        const row = setupRow(i);
-        grid.push(row);
+        return <div><Tile x={x} y={y} black={black}>{piece}</Tile></div>
     }
-    setBoard(grid);
-  }
-  
-  const setupRow = (rowNumber) => {
-    let row = [];
-    for (let i = 0; i < BOARD_SIZE; i++) {
-        if (rowNumber === 0) {
-            if (i === 0 || i === 7) {
-                row.push(<Tile child={<Rook color='black' img={BlackRook} />}></Tile>)
-            } else if (i === 1 || i === 6) {
-                row.push(<Tile child={<Knight color='black' img={BlackKnight} />}></Tile>)
-            } else if (i === 2 || i === 5) {
-                row.push(<Tile child={<Bishop color='black' img={BlackBishop} />}></Tile>)
-            } else if (i === 3) {
-                row.push(<Tile child={<Queen color='black' img={BlackQueen} />}></Tile>)
-            } else if (i === 4) {
-                row.push(<Tile child={<King color='black' img={BlackKing} />}></Tile>)
+
+    
+
+    function renderBoard() {
+        let board = [];
+      
+        for (let i = 0; i < 8; i++) {
+            let row = [];
+            for (let j = 0; j < 8; j++) {
+                row.push(renderSquare(i, j, boardPieces));
             }
-        } else if (rowNumber === 1) {
-            row.push(<Tile child={<Pawn color='black' img={BlackPawn} />}> </Tile>)
-        } else if (rowNumber === 6) {
-            row.push(<Tile child={<Pawn color='white' img={WhitePawn} />}> </Tile>)
-        } else if (rowNumber === 7) {
-            if (i === 0 || i === 7) {
-                row.push(<Tile child={<Rook color='white' img={WhiteRook} />}></Tile>)
-            } else if (i === 1 || i === 6) {
-                row.push(<Tile child={<Knight color='white' img={WhiteKnight} />}></Tile>)
-            } else if (i === 2 || i === 5) {
-                row.push(<Tile child={<Bishop color='white' img={WhiteBishop} />}></Tile>)
-            } else if (i === 3) {
-                row.push(<Tile child={<Queen color='white' img={WhiteQueen} />}></Tile>)
-            } else if (i === 4) {
-                row.push(<Tile child={<King color='white' img={WhiteKing} />}></Tile>)
-            }
-        } else {
-            row.push(<Tile child={null} position={[rowNumber, i]} key={i} />)
+            board.push(row);
         }
+        return board;
     }
-    return <div key={rowNumber} className='row-container'>{row}</div>;
-  }
-
-  return (
-    <div className='board-container'>
-        {board}
-    </div>
-  )
+    
+    
+    return (
+        <DndProvider backend={HTML5Backend}>
+            <div className="board-container">
+                {renderBoard()}
+            </div>
+        </DndProvider>
+        )
 }
