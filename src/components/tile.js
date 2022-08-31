5591122e03c3731b9b3eafae1349e29291c6df27
David <djmiller1008@yahoo.com>
2022-08-31 12:39:43 +0800

    Make tile droppable, render colors for possible drop tiles

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import { useDrop } from 'react-dnd'
+import { useDrop } from 'react-dnd';
 import '../assets/styles/tile.css'
+import { canMoveKnight, moveKnight } from './game';
 
 
-export default function Tile({ child }) {
 
-  const [, drop] = useDrop(
+
+export default function Tile({ x, y, black, children }) {
+
+  const [{ canDrop }, drop] = useDrop(
     () => ({
-      accept: 'piece'
-    })
+      accept: 'piece',
+      drop: (item) => moveKnight(x, y, item),
+      canDrop: (item) => canMoveKnight(x, y, item),
+      collect: (monitor) => ({
+        canDrop: !!monitor.canDrop(),
+       
+      })
+    }),
+    [x, y]
   )
+ 
+  const fill = black ? 'brown' : 'white';
+  const stroke = black ? 'white' : 'brown';
 
   return (
-    <div ref={drop} className='tile-container'>
-        {child}
+    <div ref={drop} style={{backgroundColor: canDrop ? 'yellow' : fill, color: stroke }} className='tile-container'>
+        {children}
     </div>
   )
 }
