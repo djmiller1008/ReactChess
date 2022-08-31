8215c3c4e9195bd7b63fb9a423ea2edad8e5f002
David <djmiller1008@yahoo.com>
2022-08-31 22:32:18 +0800

    Make knight draggable, render either white or black knight based on prop

--- src/components/pieces/knight.js
+++ src/components/pieces/knight.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd';
+import WhiteKnight from './images/whiteKnight.svg';
+import BlackKnight from './images/blackKnight.svg';
 import '../../assets/styles/tile.css';
 
-export default function Knight({ color, img }) {
-  return (
-   
-        <img className='chess-piece-image' src={img} alt='Knight' />
+export default function Knight({ pieceColor, pos }) {
  
+
+  const [{ isDragging }, drag, preview] = useDrag(() => ({
+    type: 'piece',
+    item: { pos, pieceColor },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }))
+
+  const src = pieceColor === 'white' ? WhiteKnight : BlackKnight;
+  
+  return (
+    <>
+        <DragPreviewImage connect={preview} src={src}></DragPreviewImage>
+        <img ref={drag} src={src} alt='Knight'  className='piece-image'></img>
+    </>
   )
 }
