e415b88f5c60d930725297b3bbc54bf443a6b4b7
David <djmiller1008@yahoo.com>
2022-09-02 12:23:32 +0800

    Add drag functionality to the bishop, render the image

--- src/components/pieces/bishop.js
+++ src/components/pieces/bishop.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd';
+import WhiteBishop from './images/whiteBishop.svg';
+import BlackBishop from './images/blackBishop.svg';
 import '../../assets/styles/tile.css';
 
-export default function Bishop({ color, img }) {
-  return (
-    
-        <img className='chess-piece-image' src={img} alt='Bishop' />
+export default function Bishop({ pieceColor, pos }) {
 
+  const [ ,drag, preview] = useDrag(() => ({
+    type: 'piece',
+    item: { pos, pieceColor, piece: 'bishop' }
+  }));
+
+  const src = pieceColor === 'white' ? WhiteBishop : BlackBishop;
+
+  return (
+    <>
+      <DragPreviewImage connect={preview} src={src} />
+      <img ref={drag} src={src} alt='Bishop' className='piece-image' />
+    </>
   )
 }
