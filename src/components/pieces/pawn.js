226fa43aa192690c8e102dccc148b36fa39be5c2
David <djmiller1008@yahoo.com>
2022-09-02 12:18:49 +0800

    Add black and white bishop image files

--- src/components/pieces/pawn.js
+++ src/components/pieces/pawn.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { useDrag } from 'react-dnd';
 import '../../assets/styles/tile.css';
+
 
 export default function Pawn({ color, img }) {
 
- 
-
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: 'piece',
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging()
-    })
-  }))
-
   return (
 
-      <img ref={drag} className='chess-piece-image' src={img} alt='Pawn' />
+      <img className='chess-piece-image' src={img} alt='Pawn' />
  
   )
 }
