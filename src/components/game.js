6c39fb732c69d156cbcf5acf6c0080be5913d334
David <djmiller1008@yahoo.com>
2022-09-04 10:32:24 +0800

    Add bishop and rook information to data structure, add methods so the bishop and rook can move, add methods to avoid highlighting incorrect squares

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,7 @@
 let boardPieces = {
   knight: [[0, 1, 'black'], [0, 6, 'black'], [7, 1, 'white'], [7, 6, 'white']],
-  bishop: [[0, 2, 'black'], [0, 5, 'black'], [7, 2, 'white'], [7, 5, 'white']]
+  bishop: [[0, 2, 'black'], [0, 5, 'black'], [7, 2, 'white'], [7, 5, 'white']],
+  rook: [[0, 0, 'black'], [0, 7, 'black'], [7, 0, 'white'], [7, 7, 'white']]
 }
 
 let observer = null
@@ -23,16 +24,22 @@ export function observeBoard(update) {
 export function movePiece(toX, toY, originalInfo) {
   let index;
   let pieceColor;
-  boardPieces[originalInfo.piece].forEach((pos, i) => {
+
+
+  // Loop to find the moving piece in the data struct boardPieces
+  // Find index and the piececolor 
+  boardPieces[originalInfo.piece].forEach((pos, i) => {          
     if (pos[0] === originalInfo.pos[0] && pos[1] === originalInfo.pos[1]) {
         pieceColor = pos[2];
         index = i;
+        
     }
   });
 
   
-  if (!isFriendlyPiece(toX, toY, originalInfo) && isPlayerTurn(pieceColor)) {
-    willCapturePiece(toX, toY, originalInfo);
+  if (!isFriendlyPiece(toX, toY, pieceColor) && isPlayerTurn(pieceColor)) {
+    willCapturePiece(toX, toY, originalInfo, pieceColor);
+  
     boardPieces[originalInfo.piece][index] = [toX, toY, pieceColor];
     switchTurns();
     
@@ -59,13 +66,14 @@ function switchTurns() {
   }
 }
 
-function isFriendlyPiece(toX, toY, originalInfo) {
-  const pieceColor = originalInfo.pieceColor;
+function isFriendlyPiece(toX, toY, pieceColor) {
+  
   let isFriendly = false;
   Object.keys(boardPieces).forEach(piece => {
     boardPieces[piece].forEach((pieceInfo, i) => {
-      
+    
       if (pieceInfo[2] === pieceColor && pieceInfo[0] === toX && pieceInfo[1] === toY) {
+        
         isFriendly = true;
       }
     })
@@ -74,13 +82,14 @@ function isFriendlyPiece(toX, toY, originalInfo) {
 
 }
 
-function willCapturePiece(toX, toY, originalInfo) {
+function willCapturePiece(toX, toY, pieceColor) {
  
-  const pieceColor = originalInfo.pieceColor;
+
   Object.keys(boardPieces).forEach(piece => {
-    
     boardPieces[piece].forEach((pieceInfo, i) => {
+      
       if (pieceInfo[2] !== pieceColor && pieceInfo[0] === toX && pieceInfo[1] === toY) {
+        
         const index = boardPieces[piece].indexOf(pieceInfo);
         boardPieces[piece][index] = [];
         
@@ -92,18 +101,27 @@ function willCapturePiece(toX, toY, originalInfo) {
 
 
 export function canMovePiece(toX, toY, originalInfo) {
+  let pieceColor = null;
+  boardPieces[originalInfo.piece].forEach((pos, i) => {          
+    if (pos[0] === originalInfo.pos[0] && pos[1] === originalInfo.pos[1]) {
+        pieceColor = pos[2];
+    }
+  });
+ 
   switch (originalInfo.piece) {
     case 'knight':
-      return canMoveKnight(toX, toY, originalInfo);
+      return canMoveKnight(toX, toY, originalInfo, pieceColor);
     case 'bishop':
-      return canMoveBishop(toX, toY, originalInfo);
+      return canMoveBishop(toX, toY, originalInfo, pieceColor);
+    case 'rook':
+      return canMoveRook(toX, toY, originalInfo, pieceColor);
     default:
       return;
   }
 }
 
-export function canMoveKnight(toX, toY, originalInfo) {
-    if (isFriendlyPiece(toX, toY, originalInfo)) {
+export function canMoveKnight(toX, toY, originalInfo, pieceColor) {
+    if (isFriendlyPiece(toX, toY, pieceColor)) {
       return false;
     }
     
@@ -117,16 +135,97 @@ export function canMoveKnight(toX, toY, originalInfo) {
     )
 }
 
-export function canMoveBishop(toX, toY, originalInfo) {
-  if (isFriendlyPiece(toX, toY, originalInfo)) {
+export function canMoveBishop(toX, toY, originalInfo, pieceColor) {
+  if (isFriendlyPiece(toX, toY, pieceColor)) {
     return false;
   }
 
-  const [x, y] = originalInfo.pos
+  const [x, y] = originalInfo.pos;
+  const dx = toX - x;
+  const dy = toY - y;
+
+  let xDirection = dx > 0 ? 1 : -1;
+  let yDirection = dy > 0 ? 1 : -1;
+  let blocked = false;
+  
+  if (Math.abs(dx) === Math.abs(dy)) {  // is on a diagonal
+    let stepX = x;
+    let stepY = y;
+    for (let i = 0; i < Math.abs(dx); i++) {
+      stepX = stepX + xDirection;
+      stepY = stepY + yDirection;
+      
+      if (isOccupied(stepX, stepY)) {
+      
+        if (stepX !== toX && stepY !== toY) {
+          blocked = true;
+        } 
+      }
+    }
+    return blocked === true ? false : true;
+  }
+}
+
+export function canMoveRook(toX, toY, originalInfo, pieceColor) {
+  if (isFriendlyPiece(toX, toY, pieceColor)) {
+    return false;
+  }
+
+  const [x, y] = originalInfo.pos;
   const dx = toX - x;
   const dy = toY - y;
   
-  return (Math.abs(dx) === Math.abs(dy));
+  let onStraightLine = false;
+  let blocked = false;
+  if (dx === 0 || dy === 0) {
+    onStraightLine = true;
+  }
+
+  
+  if (onStraightLine) {
+    let xDirection = 0; 
+    let yDirection = 0;
+    if (dx === 0) {
+      yDirection = dy > 0 ? 1 : -1;
+    } else {
+      xDirection = dx > 0 ? 1 : -1;
+    }
+
+    let stepX = x;
+    let stepY = y;
+    
+
+    for (let i = 0; i < (Math.abs(dx) + Math.abs(dy)); i++) {
+      
+      stepX = stepX + xDirection;
+      stepY = stepY + yDirection;
+      if (isOccupied(stepX, stepY)) {
+        if (stepX !== toX && stepY === toY) {
+          blocked = true;
+        } else if (stepX === toX && stepY !== toY) {
+          blocked = true;
+        }
+      }
+    }
+    return blocked === true ? false : true;
+  }
+  
 
 }
+  
+
+
+export function isOccupied(x, y) {
+  let occupied = false;
+  Object.keys(boardPieces).forEach((piece, i) => {
+    boardPieces[piece].forEach((pieceInfo, i) => {
+    
+      if (x === pieceInfo[0] && y === pieceInfo[1]) {
+        occupied = true;
+      }
+    })
+  });
+  return occupied;
+}
+
   
