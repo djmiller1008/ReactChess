5b123d3619671b4c6718e7f97c3907673afa595d
David <djmiller1008@yahoo.com>
2022-09-04 10:31:36 +0800

    Implement drag functionality for the rook

--- src/components/pieces/rook.js
+++ src/components/pieces/rook.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd';
+import WhiteRook from './images/whiteRook.svg';
+import BlackRook from './images/blackRook.svg';
 import '../../assets/styles/tile.css';
 
-export default function Rook({ color, img }) {
+export default function Rook({ pieceColor, pos }) {
+
+  const [ , drag, preview] = useDrag(() => ({
+    type: 'piece',
+    item: { pos, pieceColor, piece: 'rook' }
+  }));
+
+  const src = pieceColor === 'white' ? WhiteRook : BlackRook;
 
   return (
-
-    <img className='chess-piece-image' src={img} alt='Rook' />
+    <>
+      <DragPreviewImage connect={preview} src={src} />
+      <img ref={drag} className='piece-image' src={src} alt='Rook' />
+    </>
   )
 }
