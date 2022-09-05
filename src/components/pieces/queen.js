e59e7af1593a891c1b022ecf64c4390245da2874
David <djmiller1008@yahoo.com>
2022-09-05 12:36:29 +0800

    Make queen draggable, connect preview image, setup use drag hook

--- src/components/pieces/queen.js
+++ src/components/pieces/queen.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd';
+import WhiteQueen from './images/whiteQueen.svg';
+import BlackQueen from './images/blackQueen.svg';
 import '../../assets/styles/tile.css';
 
-export default function Queen({ color, img }) {
+export default function Queen({ pieceColor, pos }) {
+
+  const [ ,drag, preview] = useDrag(() => ({
+    type: 'piece',
+    item: { pos, pieceColor, piece: 'queen'}
+  }));
+
+  const src = pieceColor === 'white' ? WhiteQueen : BlackQueen;
+
   return (
-  
-    <img className='chess-piece-image' src={img} alt='Queen' />
-    
+  <>
+    <DragPreviewImage connect={preview} src={src} />
+    <img ref={drag} className='piece-image' src={src} alt='Queen' />
+  </>  
   )
 }
