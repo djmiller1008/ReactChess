27ffe24c467c3b3b943e7d3abbdcfa081f649c8b
David <djmiller1008@yahoo.com>
2022-09-06 13:29:38 +0800

    Make king draggable, connect preview image, import images

--- src/components/pieces/king.js
+++ src/components/pieces/king.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd';
+import WhiteKing from './images/whiteKing.svg';
+import BlackKing from './images/blackKing.svg';
 import '../../assets/styles/tile.css';
 
-export default function King({ color, img }) {
+export default function King({ pieceColor, pos }) {
+
+  const [ ,drag, preview] = useDrag(() => ({
+    type: 'piece',
+    item: { pos, pieceColor, piece: 'king'}
+  }));
+
+  const src = pieceColor === 'white' ? WhiteKing : BlackKing;
+
   return (
-   
-        <img className='chess-piece-image' src={img} alt='King' />
- 
+    <>
+      <DragPreviewImage connect={preview} src={src} />
+      <img ref={drag} className='piece-image' src={src} alt='King' />
+    </>
   )
 }
