7dc52262b05413913466a980a1417cf9f8c68140
David <djmiller1008@yahoo.com>
2022-09-06 13:36:56 +0800

    Make pawn draggable, connect preview image, import black and white svgs

--- src/components/pieces/pawn.js
+++ src/components/pieces/pawn.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { DragPreviewImage, useDrag } from 'react-dnd';
+import WhitePawn from './images/whitePawn.svg';
+import BlackPawn from './images/blackPawn.svg';
 import '../../assets/styles/tile.css';
 
 
-export default function Pawn({ color, img }) {
+export default function Pawn({ pieceColor, pos }) {
+
+  const [ ,drag, preview] = useDrag(() => ({
+    type: 'piece',
+    item: { pos, pieceColor, piece: 'pawn'}
+  }));
+
+  const src = pieceColor === 'white' ? WhitePawn : BlackPawn;
 
   return (
-
-      <img className='chess-piece-image' src={img} alt='Pawn' />
- 
+    <>
+      <DragPreviewImage connect={preview} src={src} />
+      <img ref={drag} className='piece-image' src={src} alt='Pawn' />
+    </>
   )
 }
